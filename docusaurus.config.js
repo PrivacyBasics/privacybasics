f0b7c72f26dc1a080d6632433cb45b7de9e94d32
Sam <asv@duck.com>
2024-06-21 19:25:03 +0530

    typ

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,7 +129,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Privacy Basics.`,
       },
       prism: {
         theme: prismThemes.github,
